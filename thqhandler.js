8affa3673e46109d2639248446b20128f7303e89
Sean McGinty <newfolderlocation@gmail.com>
2025-05-08 19:20:35 +0800

    Fix imports

--- thqhandler.js
+++ thqhandler.js
@@ -3,35 +3,36 @@ const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { authorize, authorizeWithPassword, requestAccessToken } = require("../module/src/OAuth.js");
-const TidyHQ = require("../module/index.js");
+const { authorizeWithPassword } = require("./node_modules/tidyhq/src/OAuth");
+const TidyHQ = require("tidyhq");
 
 // get environment variables
 const clientID = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-const redirectURI = process.env.REDIRECT_URI;
-const email_address = process.env.EMAIL;
-const password = process.env.PASSWORD;
-const club = process.env.CLUB;
+const email_address = process.env.AUTH_USER;
+const password = process.env.AUTH_PASS;
+const club = process.env.DOMAIN_PREFIX;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 let client;
 
 async function auth() {
     let auth = await authorizeWithPassword(clientID, clientSecret, email_address, password, club);
-    // console.log(auth)
+    console.log(auth);
     client = new TidyHQ(auth);
 }
 
 async function getGroup() {
+    client = new TidyHQ(ACCESS_TOKEN);
     // let group = await client.Groups.getGroupByName("2023-International Sundowner");
     // let group = await client.Groups.getGroupByName("Current Members");
     // let contacts = await client.Contacts.getContactsInGroup(group.id);
-    let contacts = await client.Contacts.getContactsInGroup(156920);
+    let contacts = await client.Contacts.getContactsInGroup(180107);
+    contacts = contacts.data;
     let names = [];
     for (let i = 0; i < contacts.length; i++) {
         let contact = contacts[i];
         // console.log((contact.first_name).trim() + " " + (contact.last_name).trim());
-        console.log(contact)
         names.push((contact.first_name).trim() + " " + (contact.last_name).trim());
     }
     console.log(names.length);
@@ -40,7 +41,7 @@ async function getGroup() {
 }
 
 async function main() {
-    await auth();
+    // await auth();
     await getGroup();
 }
 
